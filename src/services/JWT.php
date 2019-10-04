d061c1a387c6602fc9ae8273b344165c52ee4d83
Mike Pierce <m.pierce@edenspiekermann.com>
2019-10-04 12:11:12 +0200

    Renaming

--- src/services/JWT.php
+++ src/services/JWT.php
@@ -3,7 +3,7 @@
 /**
  * Craft JWT plugin for Craft CMS 3.x
  *
- * Enable authentication to Craft through the use of Javascript Web Tokens (JWT)
+ * Enable authentication to Craft through the use of JSON Web Tokens (JWT)
  *
  * @link      https://edenspiekermann.com
  * @copyright Copyright (c) 2019 Mike Pierce
