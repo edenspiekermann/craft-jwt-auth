3e5bc3594b03d44bab196be7d5211172cde83c13
Mike Pierce <m.pierce@edenspiekermann.com>
2019-10-06 19:00:44 +0200

    Refactor logic into services

--- src/services/JWT.php
+++ src/services/JWT.php
@@ -11,8 +11,15 @@
 
 namespace edenspiekermann\craftjwtauth\services;
 
+use Craft;
 use craft\base\Component;
+use craft\elements\User;
+use craft\helpers\StringHelper;
 use craft\helpers\ArrayHelper;
+use edenspiekermann\craftjwtauth\CraftJwtAuth;
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Token;
 
 /**
  * @author    Mike Pierce
@@ -23,6 +30,154 @@ class JWT extends Component
 {
     // Public Methods
     // =========================================================================
+
+    /*
+     * @return mixed
+     */
+    public function getJWTFromRequest()
+    {
+        // Look for an access token in the settings
+        $accessToken = Craft::$app->request->headers->get('authorization') ?: Craft::$app->request->headers->get('x-access-token');
+
+        // If "Bearer " is present, strip it to get the token.
+        if (StringHelper::startsWith($accessToken, 'Bearer ')) {
+            $accessToken = StringHelper::substr($accessToken, 7);
+        }
+
+        // If we find one, and it looks like a JWT...
+        if ($accessToken) {
+            return $accessToken;
+        }
+
+        return null;
+    }
+
+    /*
+    * @return mixed
+    */
+    public function parseAndVerifyJWT($accessToken)
+    {
+        $token = $this->parseJWT($accessToken);
+
+        if ($token && $this->verifyJWT($token)) {
+            return $token;
+        }
+
+        return null;
+    }
+
+    /*
+    * @return mixed
+    */
+    public function parseJWT($accessToken)
+    {
+        if (count(explode('.', $accessToken)) === 3) {
+            $token = (new Parser())->parse((string) $accessToken);
+
+            return $token;
+        }
+
+        return null;
+    }
+
+    /*
+    * @return mixed
+    */
+    public function verifyJWT(Token $token)
+    {
+        $secretKey = CraftJwtAuth::getInstance()->getSettings()->secretKey;
+
+        // Attempt to verify the token
+        $verify = $token->verify((new Sha256()), $secretKey);
+
+        return $verify;
+    }
+
+    /*
+    * @return mixed
+    */
+    public function getUserByJWT(Token $token)
+    {
+        if ($this->verifyJWT($token)) {
+            // Derive the username from the subject in the token
+            $email = $token->getClaim('email', '');
+            $userName = $token->getClaim('sub', '');
+
+            // Look for the user with email
+            $user = Craft::$app->users->getUserByUsernameOrEmail($email ?: $userName);
+
+            return $user;
+        }
+
+        return null;
+    }
+
+    /*
+    * @return mixed
+    */
+    public function createUserByJWT(Token $token)
+    {
+        if ($this->verifyJWT($token)) {
+            // Get relevant settings
+            $autoCreateUser = CraftJwtAuth::getInstance()->getSettings()->autoCreateUser
+                && Craft::$app->getProjectConfig()->get('users.allowPublicRegistration')
+                ?: false;
+
+            if ($autoCreateUser) {
+                // Create a new user and populate with claims
+                $user = new User();
+
+                // Email is a mandatory field
+                if ($token->hasClaim('email')) {
+                    $email = $token->getClaim('email');
+
+                    // Set username and email
+                    $user->email = $email;
+                    $user->username = $email;
+
+                    // These are optional, so pass empty string as the default
+                    $user->firstName = $token->getClaim('given_name', '');
+                    $user->lastName = $token->getClaim('family_name', '');
+
+                    // Attempt to save the user
+                    $success = Craft::$app->getElements()->saveElement($user);
+
+                    // If user saved ok...
+                    if ($success) {
+                        // Assign the user to the default public group
+                        Craft::$app->users->assignUserToDefaultGroup($user);
+
+                        // Look for a picture in the claim
+                        $picture = $token->getClaim('picture', '');
+                        if ($picture) {
+                            // Create a guzzel client
+                            $guzzle = Craft::createGuzzleClient();
+
+                            // Attempt to fetch the image
+                            $imageUpload = $guzzle->get($picture);
+
+                            // Derive the file extension from the content type
+                            $ext = self::$plugin->jWT->mime2ext($imageUpload->getHeader('Content-Type'));
+
+                            // Make a filename from the username, and add some randomness
+                            $fileName = $user->username . StringHelper::randomString() . '.' . $ext;
+                            $tempFile = Craft::$app->path->getTempAssetUploadsPath() . '/' . $fileName;
+
+                            // Fetch it again, this time saving it to a temp file
+                            $imageUpload = $guzzle->get($picture, ['save_to' => $tempFile]);
+
+                            // Save the tempfile to the user’s account as profile image
+                            Craft::$app->getUsers()->saveUserPhoto($tempFile, $user, $fileName);
+                        }
+
+                        return $user;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
 
     /*
      * @return mixed
